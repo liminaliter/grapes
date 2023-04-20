1a851d8b2d807a6561f364af629970053bf6b53d
liminaliter <liminaliter@gmail.com>
2023-04-20 23:41:28 +0900

    Rest API practice

---
+++ lib/repository/schedule_repository.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:dosomething/model/schedule_model.dart';
+import 'package:dio/dio.dart';
+
+class ScheduleRepository {
+  final _dio = Dio();
+  final _targetUrl =
+      'http://${Platform.isAndroid ? '10.0.2.2' : 'localhost'}:3000/schedule'; // Android에서는 10.0.0.2가 localhost에 해당됩니다.
+
+  Future<List<ScheduleModel>> getSchedules({
+    required DateTime date,
+  }) async {
+    final resp = await _dio.get(
+      _targetUrl,
+      queryParameters: {
+        'date':
+            '${date.year}${date.month.toString().padLeft(2, '0')}${date.day.toString().padLeft(2, '0')}',
+      },
+    );
+
+    return resp.data
+        .map<ScheduleModel>(
+          (x) => ScheduleModel.fromJson(
+            json: x,
+          ),
+        )
+        .toList();
+  }
+
+  Future<String> createSchedule({
+    required ScheduleModel schedule,
+  }) async {
+    final json = schedule.toJson();
+
+    final resp = await _dio.post(_targetUrl, data: json);
+
+    return resp.data?['id'];
+  }
+
+  Future<String> deleteSchedule({
+    required String id,
+  }) async {
+    final resp = await _dio.delete(_targetUrl, data: {
+      'id': id,
+    });
+
+    return resp.data?['id'];
+  }
+}
